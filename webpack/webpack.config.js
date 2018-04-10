f31d1c1a6c250fb00951d734f929ecfad9f794de
ttc <wudong0338@gmail.com>
2018-04-10 23:21:50 +0800

    update webpack config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // 判断当前开发环境是否是开发环境
 const isDev = process.env.NODE_ENV === 'development';
@@ -12,7 +13,7 @@ const config = {
   entry: path.join(__dirname, 'src/index.js'),
   // 编译输出文件名及文件路径
   output: {
-    filename: "[name].js",
+    filename: "[name]-[hash:8].js",
     path: path.join(__dirname, 'dist')
   },
   // 为各类型文件定义loader
@@ -22,27 +23,6 @@ const config = {
         test: /\.vue$/,
         use: ['vue-loader']
       },
-      // css文件编译顺序： css文件编译完成后再使用style编译到js代码中
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      // 同上，先将styl文件编译成css文件，添加浏览器前缀后使用cssloader文件
-      {
-        test: /\.styl$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              // 自动使用已有的source map
-              sourceMap: true
-            }
-          },
-          'stylus-loader'
-        ]
-      },
       // 处理图片，小于1024的将使用base64转码，并统一命名格式
       {
         test: /\.(jpg|jpeg|png|gif|bmp|svg)$/,
@@ -51,7 +31,7 @@ const config = {
             loader: "url-loader",
             options: {
               limit: 1024,
-              name: 'background-[name].[ext]'
+              name: '[name].[ext]'
             }
           }
         ]
@@ -71,6 +51,22 @@ const config = {
 };
 
 if (isDev) {
+  config.module.rules.push(// 样式文件编译顺序，先将styl文件编译成css文件，添加浏览器前缀后使用css编译 后使用style编译到js文件中
+    {
+      test: /\.styl$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            // 自动使用已有的source map
+            sourceMap: true
+          }
+        },
+        'stylus-loader'
+      ]
+    });
   // 可在浏览控制台source查看对应的source map
   config.devtool = "#cheap-module-eval-source-map";
   // 配置dev-server相关信息
@@ -91,8 +87,54 @@ if (isDev) {
   config.mode = "development"
 }
 else {
-  config.mode = "production"
-}
+  // 库文件单独打包
+  config.entry = {
+    main: path.join(__dirname, 'src/index.js'),
+    vendor: ['vue']
+  };
+  config.optimization = {
+    splitChunks: {
+      cacheGroups: {
+        main: {
+          name: "main"
+        },
+        vendor: {
+          name: "vendor"
+        }
+      }
+    },
+    // 避免异步加载导致其他chunk的hash发生变化而重复加载问题
+    runtimeChunk: true
+  };
+  // 使用chunkhash缓存，避免重复加载
+  config.output.filename = '[name]-[chunkhash:8].js';
+  config.mode = "production";
+  // 样式文件单独打包，依赖ExtractTextPlugin插件
+  config.module.rules.push(
+    {
+      test: /\.styl$/,
+      use: ExtractTextPlugin.extract(
+        {
+          fallback: "style-loader",
+          use:[
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            "stylus-loader"
+          ]
+        }
+      )
 
+    }
+  );
+
+  config.plugins.push(
+    new ExtractTextPlugin('styles.[chunkhash:8].css')
+  );
+}
 
 module.exports = config;
